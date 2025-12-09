343772d265135d685732d43ae550803dc5ffaecf
ddingjoo <ddingsha9@teambind.co.kr>
2025-12-09 13:00:02 +0900

    [hotfix] ProfileUpdateRequestDto Jackson 역직렬화 오류 수정
    - @NoArgsConstructor, @AllArgsConstructor 추가
    - final 키워드 제거하여 setter 접근 허용

--- springboot/src/main/java/com/mzc/backend/lms/domains/user/profile/dto/ProfileUpdateRequestDto.java
+++ springboot/src/main/java/com/mzc/backend/lms/domains/user/profile/dto/ProfileUpdateRequestDto.java
@@ -1,17 +1,21 @@
 package com.mzc.backend.lms.domains.user.profile.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 /**
  * 프로필 수정 요청 DTO
  */
 @Getter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProfileUpdateRequestDto {
 
-    private final String name;
-    private final String mobileNumber;
-    private final String homeNumber;
-    private final String officeNumber;
+    private String name;
+    private String mobileNumber;
+    private String homeNumber;
+    private String officeNumber;
 }
