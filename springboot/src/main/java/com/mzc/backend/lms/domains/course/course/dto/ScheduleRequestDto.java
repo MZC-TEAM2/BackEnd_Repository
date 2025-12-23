4823c05c9960643482fdcd8ed4c93081071b0acf
ddingjoo <ddingsha9@teambind.co.kr>
2025-12-23 13:47:16 +0900

    style: format ScheduleRequestDto.java

--- springboot/src/main/java/com/mzc/backend/lms/domains/course/course/dto/ScheduleRequestDto.java
+++ springboot/src/main/java/com/mzc/backend/lms/domains/course/course/dto/ScheduleRequestDto.java
@@ -13,9 +13,9 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ScheduleRequestDto {
-    private Integer dayOfWeek; // 1=월요일, 2=화요일, ...
-    private String startTime; // "09:00"
-    private String endTime; // "10:30"
-    private String classroom;
+	private Integer dayOfWeek; // 1=월요일, 2=화요일, ...
+	private String startTime; // "09:00"
+	private String endTime; // "10:30"
+	private String classroom;
 }
 
