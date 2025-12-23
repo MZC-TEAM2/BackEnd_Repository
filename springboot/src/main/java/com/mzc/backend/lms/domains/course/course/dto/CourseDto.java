ec834f4994614b0029d992baa40a71ded81c93b9
ddingjoo <ddingsha9@teambind.co.kr>
2025-12-23 13:47:15 +0900

    style: format CourseDto.java

--- springboot/src/main/java/com/mzc/backend/lms/domains/course/course/dto/CourseDto.java
+++ springboot/src/main/java/com/mzc/backend/lms/domains/course/course/dto/CourseDto.java
@@ -2,6 +2,7 @@ package com.mzc.backend.lms.domains.course.course.dto;
 
 import lombok.Builder;
 import lombok.Getter;
+
 import java.util.List;
 
 /**
@@ -10,15 +11,15 @@ import java.util.List;
 @Getter
 @Builder
 public class CourseDto {
-    private Long id;
-    private String courseCode;
-    private String courseName;
-    private String section;
-    private ProfessorDto professor;
-    private DepartmentDto department;
-    private Integer credits;
-    private CourseTypeDto courseType;
-    private List<ScheduleDto> schedule;
-    private String scheduleText;
-    private EnrollmentInfoDto enrollment;
+	private Long id;
+	private String courseCode;
+	private String courseName;
+	private String section;
+	private ProfessorDto professor;
+	private DepartmentDto department;
+	private Integer credits;
+	private CourseTypeDto courseType;
+	private List<ScheduleDto> schedule;
+	private String scheduleText;
+	private EnrollmentInfoDto enrollment;
 }
