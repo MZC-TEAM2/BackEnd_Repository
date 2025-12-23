8cf0664980a0c5155c9c69162e39a353b3dbaec0
ddingjoo <ddingsha9@teambind.co.kr>
2025-12-23 13:47:17 +0900

    style: format CourseTypeRepository.java

--- springboot/src/main/java/com/mzc/backend/lms/domains/course/course/repository/CourseTypeRepository.java
+++ springboot/src/main/java/com/mzc/backend/lms/domains/course/course/repository/CourseTypeRepository.java
@@ -12,23 +12,23 @@ import java.util.Optional;
 
 @Repository
 public interface CourseTypeRepository extends JpaRepository<CourseType, Long> {
-    /**
-     * 강의 유형 코드로 조회
-     */
-    Optional<CourseType> findByTypeCode(int typeCode);
-
-    /**
-     * 강의 유형 코드 존재 여부 확인
-     */
-    boolean existsByTypeCode(int typeCode);
-
-    /**
-     * 강의 유형 카테고리로 조회
-     */
-    Optional<CourseType> findByCategory(int category);
-
-    /**
-     * 강의 유형 카테고리 존재 여부 확인
-     */
-    boolean existsByCategory(int category);
+	/**
+	 * 강의 유형 코드로 조회
+	 */
+	Optional<CourseType> findByTypeCode(int typeCode);
+	
+	/**
+	 * 강의 유형 코드 존재 여부 확인
+	 */
+	boolean existsByTypeCode(int typeCode);
+	
+	/**
+	 * 강의 유형 카테고리로 조회
+	 */
+	Optional<CourseType> findByCategory(int category);
+	
+	/**
+	 * 강의 유형 카테고리 존재 여부 확인
+	 */
+	boolean existsByCategory(int category);
 }
