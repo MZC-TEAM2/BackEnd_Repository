c1bc07bdc23af10353674e39d4576cd628eba657
ddingjoo <ddingsha9@teambind.co.kr>
2025-12-23 13:47:11 +0900

    style: format InvalidRequestException.java

--- springboot/src/main/java/com/mzc/backend/lms/common/exceptions/application/InvalidRequestException.java
+++ springboot/src/main/java/com/mzc/backend/lms/common/exceptions/application/InvalidRequestException.java
@@ -9,42 +9,42 @@ import com.mzc.backend.lms.common.exceptions.CommonException;
  * HTTP 400 Bad Request
  */
 public class InvalidRequestException extends CommonException {
-
+	
 	public InvalidRequestException() {
 		super(CommonErrorCode.INVALID_INPUT);
 	}
-
+	
 	public InvalidRequestException(CommonErrorCode commonErrorCode) {
 		super(commonErrorCode);
 	}
-
+	
 	public InvalidRequestException(CommonErrorCode commonErrorCode, String message) {
 		super(commonErrorCode, message);
 	}
-
-	@Override
-	public String getExceptionType() {
-		return "APPLICATION";
-	}
-
+	
 	public static InvalidRequestException invalidFormat(String fieldName) {
 		return new InvalidRequestException(
 				CommonErrorCode.INVALID_FORMAT,
 				"잘못된 형식입니다: " + fieldName
 		);
 	}
-
+	
 	public static InvalidRequestException requiredFieldMissing(String fieldName) {
 		return new InvalidRequestException(
 				CommonErrorCode.REQUIRED_FIELD_MISSING,
 				"필수 필드가 누락되었습니다: " + fieldName
 		);
 	}
-
+	
 	public static InvalidRequestException valueOutOfRange(String fieldName, String range) {
 		return new InvalidRequestException(
 				CommonErrorCode.VALUE_OUT_OF_RANGE,
 				fieldName + "의 값이 허용 범위를 벗어났습니다: " + range
 		);
+	}
+	
+	@Override
+	public String getExceptionType() {
+		return "APPLICATION";
 	}
 }
