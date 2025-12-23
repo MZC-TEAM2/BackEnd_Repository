903cc5e6e03ac9629231123f2ed88c6c7b5e911b
ddingjoo <ddingsha9@teambind.co.kr>
2025-12-23 13:47:24 +0900

    style: format UserSearchCursorResponseDto.java

--- springboot/src/main/java/com/mzc/backend/lms/domains/user/search/dto/UserSearchCursorResponseDto.java
+++ springboot/src/main/java/com/mzc/backend/lms/domains/user/search/dto/UserSearchCursorResponseDto.java
@@ -11,42 +11,42 @@ import java.util.List;
 @Getter
 @Builder
 public class UserSearchCursorResponseDto {
-
-    private List<UserSearchResponseDto> content;
-
-    private Long nextCursorId;
-
-    private String nextCursorName;
-
-    private boolean hasNext;
-
-    public static UserSearchCursorResponseDto of(
-            List<UserSearchResponseDto> content,
-            int requestedSize,
-            UserSearchRequestDto.SortBy sortBy
-    ) {
-        boolean hasNext = content.size() > requestedSize;
-
-        List<UserSearchResponseDto> resultContent = hasNext
-                ? content.subList(0, requestedSize)
-                : content;
-
-        Long nextCursorId = null;
-        String nextCursorName = null;
-
-        if (!resultContent.isEmpty()) {
-            UserSearchResponseDto last = resultContent.get(resultContent.size() - 1);
-            nextCursorId = last.getUserId();
-            if (sortBy == UserSearchRequestDto.SortBy.NAME) {
-                nextCursorName = last.getName();
-            }
-        }
-
-        return UserSearchCursorResponseDto.builder()
-                .content(resultContent)
-                .nextCursorId(nextCursorId)
-                .nextCursorName(nextCursorName)
-                .hasNext(hasNext)
-                .build();
-    }
+	
+	private List<UserSearchResponseDto> content;
+	
+	private Long nextCursorId;
+	
+	private String nextCursorName;
+	
+	private boolean hasNext;
+	
+	public static UserSearchCursorResponseDto of(
+			List<UserSearchResponseDto> content,
+			int requestedSize,
+			UserSearchRequestDto.SortBy sortBy
+	) {
+		boolean hasNext = content.size() > requestedSize;
+		
+		List<UserSearchResponseDto> resultContent = hasNext
+				? content.subList(0, requestedSize)
+				: content;
+		
+		Long nextCursorId = null;
+		String nextCursorName = null;
+		
+		if (!resultContent.isEmpty()) {
+			UserSearchResponseDto last = resultContent.get(resultContent.size() - 1);
+			nextCursorId = last.getUserId();
+			if (sortBy == UserSearchRequestDto.SortBy.NAME) {
+				nextCursorName = last.getName();
+			}
+		}
+		
+		return UserSearchCursorResponseDto.builder()
+				.content(resultContent)
+				.nextCursorId(nextCursorId)
+				.nextCursorName(nextCursorName)
+				.hasNext(hasNext)
+				.build();
+	}
 }
