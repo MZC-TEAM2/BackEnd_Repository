2ec04d5c71199ce4c7aa752bf830bbf58f071798
ddingjoo <ddingsha9@teambind.co.kr>
2025-12-23 13:47:14 +0900

    style: format PostHashtag.java

--- springboot/src/main/java/com/mzc/backend/lms/domains/board/entity/PostHashtag.java
+++ springboot/src/main/java/com/mzc/backend/lms/domains/board/entity/PostHashtag.java
@@ -15,45 +15,45 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class PostHashtag extends AuditableEntity {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
-    private Long id;
-
-    /**
-     * 게시글
-     */
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "post_id", nullable = false)
-    private Post post;
-
-    /**
-     * 해시태그
-     */
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "hashtag_id", nullable = false)
-    private Hashtag hashtag;
-
-    @Builder
-    public PostHashtag(Post post, Hashtag hashtag, Long createdBy) {
-        super(createdBy);
-        this.post = post;
-        this.hashtag = hashtag;
-    }
-
-    // --- 연관관계 편의 메서드 ---
-
-    /**
-     * 게시글 설정 (양방향 관계 설정)
-     */
-    public void setPost(Post post) {
-        if (this.post != null) {
-            this.post.getPostHashtags().remove(this);
-        }
-        this.post = post;
-        if (post != null && !post.getPostHashtags().contains(this)) {
-            post.getPostHashtags().add(this);
-        }
-    }
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long id;
+	
+	/**
+	 * 게시글
+	 */
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "post_id", nullable = false)
+	private Post post;
+	
+	/**
+	 * 해시태그
+	 */
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "hashtag_id", nullable = false)
+	private Hashtag hashtag;
+	
+	@Builder
+	public PostHashtag(Post post, Hashtag hashtag, Long createdBy) {
+		super(createdBy);
+		this.post = post;
+		this.hashtag = hashtag;
+	}
+	
+	// --- 연관관계 편의 메서드 ---
+	
+	/**
+	 * 게시글 설정 (양방향 관계 설정)
+	 */
+	public void setPost(Post post) {
+		if (this.post != null) {
+			this.post.getPostHashtags().remove(this);
+		}
+		this.post = post;
+		if (post != null && !post.getPostHashtags().contains(this)) {
+			post.getPostHashtags().add(this);
+		}
+	}
 }
