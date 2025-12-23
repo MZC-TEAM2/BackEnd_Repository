ea8b3289ff7ad3d947860a5ca8448f2c593d268b
ddingjoo <ddingsha9@teambind.co.kr>
2025-12-23 13:47:19 +0900

    style: format CourseCart.java

--- springboot/src/main/java/com/mzc/backend/lms/domains/enrollment/entity/CourseCart.java
+++ springboot/src/main/java/com/mzc/backend/lms/domains/enrollment/entity/CourseCart.java
@@ -1,23 +1,14 @@
 package com.mzc.backend.lms.domains.enrollment.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.Column;
-import jakarta.persistence.Table;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import com.mzc.backend.lms.domains.course.course.entity.Course;
+import com.mzc.backend.lms.domains.user.student.entity.Student;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
-
-import com.mzc.backend.lms.domains.user.student.entity.Student;
-import com.mzc.backend.lms.domains.course.course.entity.Course;
 
 
 /*
@@ -28,21 +19,22 @@ import com.mzc.backend.lms.domains.course.course.entity.Course;
 @Entity
 @Table(name = "course_carts")
 @Getter
-@NoArgsConstructor @AllArgsConstructor
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class CourseCart {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "student_id", nullable = false)
-    private Student student; // 학생 ID
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "course_id", nullable = false)
-    private Course course; // 강의 ID
-
-    @Column(name = "added_at", nullable = false)
-    private LocalDateTime addedAt; // 담은 일시
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "student_id", nullable = false)
+	private Student student; // 학생 ID
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "course_id", nullable = false)
+	private Course course; // 강의 ID
+	
+	@Column(name = "added_at", nullable = false)
+	private LocalDateTime addedAt; // 담은 일시
 }
