7873f9d0b1ecacd601b05a208c0d7754194f4be7
ddingjoo <ddingsha9@teambind.co.kr>
2025-12-23 13:47:12 +0900

    style: format AssessmentType.java

--- springboot/src/main/java/com/mzc/backend/lms/domains/assessment/enums/AssessmentType.java
+++ springboot/src/main/java/com/mzc/backend/lms/domains/assessment/enums/AssessmentType.java
@@ -4,10 +4,10 @@ package com.mzc.backend.lms.domains.assessment.enums;
  * 시험/퀴즈 유형 (exams.exam_type)
  */
 public enum AssessmentType {
-    MIDTERM,
-    FINAL,
-    QUIZ,
-    REGULAR
+	MIDTERM,
+	FINAL,
+	QUIZ,
+	REGULAR
 }
 
 
