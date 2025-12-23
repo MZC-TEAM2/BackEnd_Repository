d48dc80967708e914584483e2b963c866f22e5e6
ddingjoo <ddingsha9@teambind.co.kr>
2025-12-23 13:47:24 +0900

    style: format ImageStorageException.java

--- springboot/src/main/java/com/mzc/backend/lms/util/image/ImageStorageException.java
+++ springboot/src/main/java/com/mzc/backend/lms/util/image/ImageStorageException.java
@@ -4,12 +4,12 @@ package com.mzc.backend.lms.util.image;
  * 이미지 저장 관련 예외
  */
 public class ImageStorageException extends RuntimeException {
-
-    public ImageStorageException(String message) {
-        super(message);
-    }
-
-    public ImageStorageException(String message, Throwable cause) {
-        super(message, cause);
-    }
+	
+	public ImageStorageException(String message) {
+		super(message);
+	}
+	
+	public ImageStorageException(String message, Throwable cause) {
+		super(message, cause);
+	}
 }
