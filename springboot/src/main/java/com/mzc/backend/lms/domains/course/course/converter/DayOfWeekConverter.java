8f683620ed08ca510ca8e522d63e294a6484bc9c
ddingjoo <ddingsha9@teambind.co.kr>
2025-12-23 13:47:15 +0900

    style: format DayOfWeekConverter.java

--- springboot/src/main/java/com/mzc/backend/lms/domains/course/course/converter/DayOfWeekConverter.java
+++ springboot/src/main/java/com/mzc/backend/lms/domains/course/course/converter/DayOfWeekConverter.java
@@ -2,24 +2,25 @@ package com.mzc.backend.lms.domains.course.course.converter;
 
 import jakarta.persistence.AttributeConverter;
 import jakarta.persistence.Converter;
+
 import java.time.DayOfWeek;
 
 @Converter(autoApply = true)
 public class DayOfWeekConverter implements AttributeConverter<DayOfWeek, Integer> {
-    
-    @Override
-    public Integer convertToDatabaseColumn(DayOfWeek dayOfWeek) {
-        if (dayOfWeek == null) {
-            return null;
-        }
-        return dayOfWeek.getValue(); // MONDAY=1, TUESDAY=2, ..., FRIDAY=5
-    }
-    
-    @Override
-    public DayOfWeek convertToEntityAttribute(Integer dbData) {
-        if (dbData == null) {
-            return null;
-        }
-        return DayOfWeek.of(dbData); // 1=MONDAY, 2=TUESDAY, ..., 5=FRIDAY
-    }
+	
+	@Override
+	public Integer convertToDatabaseColumn(DayOfWeek dayOfWeek) {
+		if (dayOfWeek == null) {
+			return null;
+		}
+		return dayOfWeek.getValue(); // MONDAY=1, TUESDAY=2, ..., FRIDAY=5
+	}
+	
+	@Override
+	public DayOfWeek convertToEntityAttribute(Integer dbData) {
+		if (dbData == null) {
+			return null;
+		}
+		return DayOfWeek.of(dbData); // 1=MONDAY, 2=TUESDAY, ..., 5=FRIDAY
+	}
 }
