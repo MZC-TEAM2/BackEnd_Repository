26a605e44f40b1e6f603ea7d99fb1b04ac16071e
ddingjoo <ddingsha9@teambind.co.kr>
2025-12-23 13:47:22 +0900

    style: format VerifyCodeRequestDto.java

--- springboot/src/main/java/com/mzc/backend/lms/domains/user/auth/dto/VerifyCodeRequestDto.java
+++ springboot/src/main/java/com/mzc/backend/lms/domains/user/auth/dto/VerifyCodeRequestDto.java
@@ -10,12 +10,12 @@ import lombok.Data;
  */
 @Data
 public class VerifyCodeRequestDto {
-
-    @NotBlank(message = "이메일은 필수입니다")
-    @Email(message = "올바른 이메일 형식이 아닙니다")
-    private String email;
-
-    @NotBlank(message = "인증 코드는 필수입니다")
-    @Size(min = 5, max = 5, message = "인증 코드는 5자리여야 합니다")
-    private String code;
+	
+	@NotBlank(message = "이메일은 필수입니다")
+	@Email(message = "올바른 이메일 형식이 아닙니다")
+	private String email;
+	
+	@NotBlank(message = "인증 코드는 필수입니다")
+	@Size(min = 5, max = 5, message = "인증 코드는 5자리여야 합니다")
+	private String code;
 }
