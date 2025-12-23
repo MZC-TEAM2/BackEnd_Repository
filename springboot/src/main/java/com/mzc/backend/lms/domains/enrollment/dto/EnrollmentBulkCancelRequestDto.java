bdd26a4a0c86f272ed863d20ccec6dd388ec5d28
ddingjoo <ddingsha9@teambind.co.kr>
2025-12-23 13:47:19 +0900

    style: format EnrollmentBulkCancelRequestDto.java

--- springboot/src/main/java/com/mzc/backend/lms/domains/enrollment/dto/EnrollmentBulkCancelRequestDto.java
+++ springboot/src/main/java/com/mzc/backend/lms/domains/enrollment/dto/EnrollmentBulkCancelRequestDto.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
 import java.util.List;
 
 /**
@@ -14,5 +15,5 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 public class EnrollmentBulkCancelRequestDto {
-    private List<Long> enrollmentIds;
+	private List<Long> enrollmentIds;
 }
