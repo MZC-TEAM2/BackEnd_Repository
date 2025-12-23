4b2428c6e4ef3ab8003cff046ab688f8ade38c2e
ddingjoo <ddingsha9@teambind.co.kr>
2025-12-23 13:47:13 +0900

    style: format CommentUpdateRequestDto.java

--- springboot/src/main/java/com/mzc/backend/lms/domains/board/dto/request/CommentUpdateRequestDto.java
+++ springboot/src/main/java/com/mzc/backend/lms/domains/board/dto/request/CommentUpdateRequestDto.java
@@ -1,11 +1,12 @@
 package com.mzc.backend.lms.domains.board.dto.request;
 
-import java.util.List;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 /**
  * 댓글 수정 요청 DTO
@@ -15,13 +16,13 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class CommentUpdateRequestDto {
-
-    @NotBlank(message = "내용은 필수입니다")
-    private String content;
-
-    // 새로 추가할 첨부파일 ID 목록
-    private List<Long> attachmentIds;
-
-    // 삭제할 첨부파일 ID 목록
-    private List<Long> removedAttachmentIds;
+	
+	@NotBlank(message = "내용은 필수입니다")
+	private String content;
+	
+	// 새로 추가할 첨부파일 ID 목록
+	private List<Long> attachmentIds;
+	
+	// 삭제할 첨부파일 ID 목록
+	private List<Long> removedAttachmentIds;
 }
