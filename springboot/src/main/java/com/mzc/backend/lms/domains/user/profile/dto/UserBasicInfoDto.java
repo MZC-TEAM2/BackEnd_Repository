933404125abdf3b913720b9861711790429cd99f
ddingjoo <ddingsha9@teambind.co.kr>
2025-12-23 13:47:23 +0900

    style: format UserBasicInfoDto.java

--- springboot/src/main/java/com/mzc/backend/lms/domains/user/profile/dto/UserBasicInfoDto.java
+++ springboot/src/main/java/com/mzc/backend/lms/domains/user/profile/dto/UserBasicInfoDto.java
@@ -14,24 +14,24 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserBasicInfoDto {
-
-    private Long id;            // 학번 또는 교번 (userId와 동일)
-    private String name;        // 복호화된 이름
-    private String userType;    // STUDENT, PROFESSOR
-
-    public static UserBasicInfoDto ofStudent(Long id, String name) {
-        return UserBasicInfoDto.builder()
-                .id(id)
-                .name(name)
-                .userType("STUDENT")
-                .build();
-    }
-
-    public static UserBasicInfoDto ofProfessor(Long id, String name) {
-        return UserBasicInfoDto.builder()
-                .id(id)
-                .name(name)
-                .userType("PROFESSOR")
-                .build();
-    }
+	
+	private Long id;            // 학번 또는 교번 (userId와 동일)
+	private String name;        // 복호화된 이름
+	private String userType;    // STUDENT, PROFESSOR
+	
+	public static UserBasicInfoDto ofStudent(Long id, String name) {
+		return UserBasicInfoDto.builder()
+				.id(id)
+				.name(name)
+				.userType("STUDENT")
+				.build();
+	}
+	
+	public static UserBasicInfoDto ofProfessor(Long id, String name) {
+		return UserBasicInfoDto.builder()
+				.id(id)
+				.name(name)
+				.userType("PROFESSOR")
+				.build();
+	}
 }
