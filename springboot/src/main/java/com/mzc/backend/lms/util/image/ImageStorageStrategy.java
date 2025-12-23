8159b71f84a81e3ab5af6e1b6fa19892b5d10509
ddingjoo <ddingsha9@teambind.co.kr>
2025-12-23 13:47:24 +0900

    style: format ImageStorageStrategy.java

--- springboot/src/main/java/com/mzc/backend/lms/util/image/ImageStorageStrategy.java
+++ springboot/src/main/java/com/mzc/backend/lms/util/image/ImageStorageStrategy.java
@@ -7,35 +7,39 @@ import java.io.InputStream;
  * Local, S3 등 다양한 저장소 구현 가능
  */
 public interface ImageStorageStrategy {
-
-    /**
-     * 이미지 저장
-     * @param inputStream 이미지 입력 스트림
-     * @param fileName 저장할 파일명 (ULID 기반)
-     * @param contentType 컨텐츠 타입
-     * @return 저장된 이미지 URL
-     */
-    String store(InputStream inputStream, String fileName, String contentType);
-
-    /**
-     * 이미지 저장 (바이트 배열)
-     * @param data 이미지 바이트 배열
-     * @param fileName 저장할 파일명
-     * @param contentType 컨텐츠 타입
-     * @return 저장된 이미지 URL
-     */
-    String store(byte[] data, String fileName, String contentType);
-
-    /**
-     * 이미지 삭제
-     * @param fileUrl 삭제할 이미지 URL
-     */
-    void delete(String fileUrl);
-
-    /**
-     * URL에서 파일명 추출
-     * @param fileUrl 파일 URL
-     * @return 파일명
-     */
-    String extractFileName(String fileUrl);
+	
+	/**
+	 * 이미지 저장
+	 *
+	 * @param inputStream 이미지 입력 스트림
+	 * @param fileName    저장할 파일명 (ULID 기반)
+	 * @param contentType 컨텐츠 타입
+	 * @return 저장된 이미지 URL
+	 */
+	String store(InputStream inputStream, String fileName, String contentType);
+	
+	/**
+	 * 이미지 저장 (바이트 배열)
+	 *
+	 * @param data        이미지 바이트 배열
+	 * @param fileName    저장할 파일명
+	 * @param contentType 컨텐츠 타입
+	 * @return 저장된 이미지 URL
+	 */
+	String store(byte[] data, String fileName, String contentType);
+	
+	/**
+	 * 이미지 삭제
+	 *
+	 * @param fileUrl 삭제할 이미지 URL
+	 */
+	void delete(String fileUrl);
+	
+	/**
+	 * URL에서 파일명 추출
+	 *
+	 * @param fileUrl 파일 URL
+	 * @return 파일명
+	 */
+	String extractFileName(String fileUrl);
 }
